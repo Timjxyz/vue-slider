b8b864b913f11f0d02589ca25147dd865e5a9a1c
Timothy Gaither <timjgaither@gmail.com>
2022-02-10 16:12:04 +0100

    push primo bonus

---
+++ main.js
@@ -0,0 +1,56 @@
+const vue= new Vue({
+    el:'#app',
+
+    data:{
+
+        active:0,
+
+        items: [
+            {
+                url:'img/01.jpg',
+                name:'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                url:'img/02.jpg',
+                name:'Svizzera',
+                text:'Lorem ipsum'
+            },
+            {
+                url:'img/03.jpg',
+                name:'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                url:'img/04.jpg',
+                name:'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            },
+            {
+                url:'img/05.jpg',
+                name:'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            }
+        ],
+    },
+
+    methods:{
+
+        prev:function(){
+            if(this.active==0){
+                this.active=this.items.length-1;         
+            }else{
+                this.active--;
+            }
+        },
+
+        next:function(){
+            if(this.active==this.items.length-1){
+                this.active=0;         
+            }else{
+                this.active++;
+            }
+        },
+    }
+})
+
